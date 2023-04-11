1dca1062677b76895eeaad0379cc5996c4e3351b
Teona <tekizashvili@gmail.com>
2023-04-11 20:20:41 +0400

    fetched questions

--- src/context.jsx
+++ src/context.jsx
@@ -1,4 +1,5 @@
-import { useContext, createContext, useState } from 'react';
+import axios from 'axios';
+import { useContext, createContext, useState, useEffect } from 'react';
 
 const API_ENDPOINT = 'https://opentdb.com/api.php?';
 
@@ -17,6 +18,31 @@ const AppProvider = ({ children }) => {
   const [correct, setCorrect] = useState(0);
   const [error, setError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => console.log(err));
+
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setWaiting(false);
+        setLoading(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
 
   return (
     <AppContext.Provider
