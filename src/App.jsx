13d88d04affb6bb6555ffc9b0bacc22a9aa3f2c1
Teona <tekizashvili@gmail.com>
2023-04-11 21:28:40 +0400

    added displaying questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useGlobalContext } from './context';
 import SetupForm from './SetupForm';
 import Loading from './Loading';
+// import Modal from './Modal';
 
 function App() {
   const { waiting, loading, questions, index, correct } = useGlobalContext();
@@ -13,7 +14,33 @@ function App() {
     return <Loading />;
   }
 
-  return <h1>hi</h1>;
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          question {index} out of {questions.length}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  className="answer-btn"
+                  key={index}
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                ></button>
+              );
+            })}
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
